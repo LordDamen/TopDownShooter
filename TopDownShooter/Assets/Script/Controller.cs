5404b02a38a2675a1d57fadb5dffedf28b46dfd6
Benjamin Chouteau <35677618+LordDamen@users.noreply.github.com>
2019-01-09 17:37:31 -0700

    Added The animation Controler

---
+++ TopDownShooter/Assets/Script/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+    public Pawn pawn;
+	// Use this for initialization
+	void Start () {
+        pawn = GetComponent<Pawn>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        pawn.Move(moveDirection);
+	}
+}
---
+++ TopDownShooter/Assets/Script/Pawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pawn : MonoBehaviour {
+
+     public Animator anim;
+
+        void Start()
+        {
+            anim = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        public void Move(Vector3 direction)
+        {
+            anim.SetFloat("Vertical", direction.x);
+            anim.SetFloat("Horizontal", direction.z);
+        }
+}
