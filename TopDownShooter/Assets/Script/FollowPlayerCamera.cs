dbea0dfa2b0675b835d35722d3bf3d002a473038
Benjamin Chouteau <35677618+LordDamen@users.noreply.github.com>
2019-01-14 17:21:35 -0700

    started work on animations

---
+++ TopDownShooter/Assets/Script/FollowPlayerCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerCamera : MonoBehaviour {
+    public Transform targetObjectTransform;
+    public Vector3 offset;
+    private Transform tf;
+	// Use this for initialization
+	void Start () {
+        tf = gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tf.position = targetObjectTransform.position + offset;
+        tf.LookAt(targetObjectTransform.position);
+	}
+}
