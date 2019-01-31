49a43e742183b4236e9f06f012c4e361150e79a7
Benjamin Chouteau <35677618+LordDamen@users.noreply.github.com>
2019-01-30 17:43:21 -0700

    0.0.3
    Need to do the video

--- TopDownShooter/Assets/Scripts/Controller.cs
+++ TopDownShooter/Assets/Scripts/Controller.cs
@@ -2,43 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour {
+public class Controller : MonoBehaviour
+{
 
     [SerializeField]
     public Pawn pawn;
-	
+
     // Use this for initialization
-	void Start () {
+    void Start()
+    {
         pawn = GetComponent<Pawn>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         Rotation();
         Movement();
-	}
+    }
     void Rotation()
     {
         //create a plane object
-        Plane theplane = new Plane(Vector3.up,pawn.tf.position);
+        Plane theplane = new Plane(Vector3.up, pawn.tf.position);
         //raycast out the camera at mouse position to plane --- find the distance to the plane
         float distance;
         Ray theRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         theplane.Raycast(theRay, out distance);
         //find a point on the ray thats is the "distance " down the ray
-        Vector3 targetPoint =theRay.GetPoint(distance);
+        Vector3 targetPoint = theRay.GetPoint(distance);
         // Rotate to look at that point
         pawn.tf.LookAt(targetPoint);
     }
-    void Movement ()
+    void Movement()
     {
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         // a vector cant be longer than this
         moveDirection = Vector3.ClampMagnitude(moveDirection, 1.0f);
-        Debug.Log(moveDirection);
         // for world direction
         moveDirection = pawn.tf.InverseTransformDirection(moveDirection);
-        
+
         pawn.Move(moveDirection);
+        if (Input.GetKey(pawn.runningKey) || pawn.isTired)
+        {
+            pawn.Running();
+        } else
+        {
+            pawn.anim.SetBool("IsRunning", false);
+        }
     }
 }
---
+++ TopDownShooter/Assets/Scripts/Pawn.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pawn : MonoBehaviour {
+
+    [SerializeField]
+    public Animator anim;
+    [SerializeField]
+    public Transform tf;
+    [SerializeField]
+    public float turnSpeed;
+    [SerializeField]
+    public float moveSpeed;
+    [SerializeField]
+    public float stamina = 1f;
+    [SerializeField]
+    public float staminaDepleteTimer = 5f;
+    [SerializeField]
+    public float staminaRegenTimer = 3f;
+    [SerializeField]
+    public KeyCode runningKey;
+    [SerializeField]
+    public bool isTired = false;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        tf = GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Move(Vector3 direction)
+    {
+        anim.SetFloat("Vertical", direction.z );
+        anim.SetFloat("Horizontal", direction.x);
+    }
+    public void RotateTowards(Vector3 targetPoint)
+    {
+        Vector3 toLookDown = targetPoint - tf.position ;
+        Quaternion lookRotation =  Quaternion.LookRotation(toLookDown, tf.up);
+        tf.rotation = Quaternion.RotateTowards(tf.rotation, lookRotation, turnSpeed * Time.deltaTime);
+    }
+    public void Running()
+    {
+        if (isTired == false)
+        {
+            stamina -= Time.deltaTime / staminaDepleteTimer;
+            anim.SetBool("IsRunning", true);
+        } 
+        if (stamina <=0 || isTired == true)
+        {
+            isTired = true;
+            stamina += Time.deltaTime / staminaRegenTimer;
+            anim.SetBool("IsRunning", false);
+            Debug.Log("added stam");
+        }
+        stamina = Mathf.Clamp01(stamina);
+        if (isTired == true && stamina >= 1f) isTired = false;
+    }
+}
