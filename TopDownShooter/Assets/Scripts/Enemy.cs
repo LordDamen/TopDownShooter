594fc65344e82f3fb58dc121def603904f1955c5
Devon Thomsen <devon.thomsen@gmail.com>
2019-03-31 13:08:49 -0700

    Enimes added
    just need to add ragdoll

---
+++ TopDownShooter/Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent TheMeshAgent;
+    public GameObject target;
+    private Transform character;
+    public Pawn pawn;
+    public float moveSpeed;
+    public float maxRange;
+    private Animator anim;
+    private GameManager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+        TheMeshAgent = GetComponent<NavMeshAgent>();
+        pawn = GetComponent<Pawn>();
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+    // This is the function that allows for the enemy to move
+    public void Movement ()
+    {
+        // setting the target to the player
+        TheMeshAgent.SetDestination(target.transform.position);
+        //converting the target to a vector 3
+        Vector3 desiredVelocity = TheMeshAgent.desiredVelocity * moveSpeed;
+        Vector3 Input = Vector3.MoveTowards(desiredVelocity, TheMeshAgent.desiredVelocity, TheMeshAgent.acceleration * Time.deltaTime);
+        Input = transform.InverseTransformDirection(Input);
+        anim.SetFloat("Horizontal", Input.x);
+        anim.SetFloat("Vertical", Input.z);
+    }
+    private void OnAnimatorMove()
+    {
+        TheMeshAgent.velocity = anim.velocity;
+    }
+}
---
+++ TopDownShooter/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public GameObject Player;
+    private SceneManager sm;
+    void awake()
+    {
+        if(instance == null)
+        {
+            instance = this; 
+        } else
+        {
+            Destroy(gameObject);
+        }
+        Player = GameObject.FindGameObjectsWithTag("Player")[0];
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnSceneLoad()
+    {
+        Player = GameObject.FindGameObjectsWithTag("Player")[0];
+    }
+}
