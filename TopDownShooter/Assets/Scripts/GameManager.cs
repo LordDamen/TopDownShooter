bc46a49ad2f3be9819c5657d1ff6913daf7be9d6
Devon Thomsen <devon.thomsen@gmail.com>
2019-04-11 19:05:13 -0700

    Project 4 done
    just need to recored the video

--- TopDownShooter/Assets/Scripts/GameManager.cs
+++ TopDownShooter/Assets/Scripts/GameManager.cs
@@ -8,6 +8,8 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public GameObject Player;
     private SceneManager sm;
+    public GameObject pauseMenu;
+    private bool paused;
     void awake()
     {
         if(instance == null)
@@ -18,20 +20,42 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
         Player = GameObject.FindGameObjectsWithTag("Player")[0];
+        pauseMenu = GameObject.Find("Pause Menu");
+        pauseMenu.SetActive(false);
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        pauseMenu = GameObject.Find("Pause Menu");
+        pauseMenu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape) )
+        {
+            Pause();
 
+        }
     }
     void OnSceneLoad()
     {
         Player = GameObject.FindGameObjectsWithTag("Player")[0];
+        
+    }
+    public void Pause()
+    {
+        if (!paused)
+        {
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+            
+        } else
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+        }
+        paused = !paused;
     }
 }
